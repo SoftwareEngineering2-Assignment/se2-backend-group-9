669accdc4c9d217fb5655da72e9bc27c6b13d204
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-03 19:47:49 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-unresolved */
-require('dotenv').config();
+require('dotenv').config({ path: 'env' });
 
 const http = require('node:http');
 const test = require('ava').default;
