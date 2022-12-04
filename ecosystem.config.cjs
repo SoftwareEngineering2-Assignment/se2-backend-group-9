21c0735eba7925135dc0bb3a10cd7e7b8a0f7861
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-04 22:15:49 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,13 +6,6 @@ module.exports = {
       args: 'start',
       env: {
         PORT: 3009,
-        SERVER_SECRET: ${{ secrets.SERVER_SECRET }},
-        MONGODB_URI: ${{ secrets.MONGODB_URI }},
-        PLATFORM_URI: 'http://localhost:3002',
-        SERVER_URI: 'http://localhost:3000',
-        SERVER_EMAIL: 'karanikio@auth.gr',
-        SENDGRID_API_KEY: ${{ secrets.SENDGRID_API_KEY }}
-      },
     },
   ],
 };
