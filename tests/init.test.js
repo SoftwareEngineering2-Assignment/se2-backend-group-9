49c5ddb0ee56a96bc8c9a83653093446bc8500be
ConstandinosA <constandinosandreou9@gmail.com>
2022-12-06 12:23:23 +0200

    M

--- tests/init.test.js
+++ tests/init.test.js
@@ -19,6 +19,7 @@ test.after.always((t) => {
   t.context.server.close();
 });
 
+/*Test for the response and status code of get statitics */
 test('GET /statistics returns correct response and status code', async (t) => {
   const {body, statusCode} = await t.context.got('general/statistics');
   t.is(body.sources, 0);
@@ -26,12 +27,14 @@ test('GET /statistics returns correct response and status code', async (t) => {
   t.is(statusCode, 200);
 });
 
+/*Test for the response and status code of get sources */
 test('GET /sources returns correct response and status code', async (t) => {
   const token = jwtSign({id: 1});
   const {statusCode} = await t.context.got(`sources/sources?token=${token}`);
   t.is(statusCode, 200);
 });
 
+/*Test for the response and status code of get dashboard */
 test('GET /dashboard returns correct response and status code', async (t) => {
   const token = jwtSign({id: 1});
   const {body, statusCode} = await t.context.got(`dashboards/dashboards?token=${token}`);
@@ -39,6 +42,7 @@ test('GET /dashboard returns correct response and status code', async (t) => {
   t.is(statusCode, 200);
 });
 
+/*Test for post request of register account with existing or user or email */
 test('POST /create should return error if email or user exists', async t => {
   const username = 'dummy';
   const password = '12345678';
@@ -50,6 +54,7 @@ test('POST /create should return error if email or user exists', async t => {
   t.is(data.status, 409);
 })
 
+/*Test for user authentication (post) */
 test('POST /authenticate authenticates correctly a dummy user', async t => {
   
   //change to secrets
@@ -62,3 +67,60 @@ test('POST /authenticate authenticates correctly a dummy user', async t => {
       }).json();
   t.is(data.user.username, username);
 })
+
+/*Test for user authentication if password is wrong (post) */
+test('POST /authenticate a dummy user with wrong pasword', async t => {
+  
+  //change to secrets
+  
+  const username = 'dummy';
+  const password = '135790';
+  
+  const data = await t.context.got.post(`users/authenticate`, {
+        json: { username, password }
+      }).json();
+  t.is(data.status, 401);
+})
+
+/*Test for user authentication if username is wrong (post) */
+test('POST /authenticate a dummy user with wrong username', async t => {
+  
+  //change to secrets
+  
+  const username = 'dummmmy';
+  const password = '123456789';
+  
+  const data = await t.context.got.post(`users/authenticate`, {
+        json: { username, password }
+      }).json();
+  t.is(data.status, 401);
+})
+
+/* Test for paswword reset if user types wrong username
+test('POST /resetpassword reset password with wrong username', async t => {
+  
+  //change to secrets
+  
+  const username = 'dummmmy';
+
+  const data = await t.context.got.post(`/resetpassword`, {
+        json: { username }
+      }).json();
+  t.is(data.status, 404);
+})*/
+
+
+
+/*Test for paswword change if user types wrong username
+test('POST /changepassword change password with wrong username', async t => {
+  
+  //change to secrets
+  
+  const username = 'dummy';
+  const password = '123456789';
+
+  const data = await t.context.got.post(`users/changepassword`, {
+        json: { username, password }
+      }).json();
+  t.is(data.status, 404);
+})*/
