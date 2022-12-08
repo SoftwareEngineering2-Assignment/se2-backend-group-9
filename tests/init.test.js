41e20368daa64c40e39427650dc2c207cada203f
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-08 15:35:53 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -14,7 +14,7 @@ const dashboard0ID = '6391d8972c8c733c64857525';
 const wrongdashID = '6390be757de6d2fa567a3e34';
 
 require('dotenv').config(app.env);
-const authToken = AUTH_TOKEN;
+const authToken = app.env.AUTH_TOKEN;
 //console.log(process.env);
 
 /* 
