bd739a1a8c706a305fa407b074338ef2d7dde88f
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-08 14:37:43 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -11,10 +11,10 @@ const { jwtSign } = require('../src/utilities/authentication/helpers');
 const { AssertionError } = require('node:assert');
 const { isAsyncFunction } = require('node:util/types');
 const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15IiwiaWQiOiI2MzhlNDA5MTMxMmRiMTRjNmVjMzBlNmYiLCJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImlhdCI6MTY3MDQxNjE5M30.g4hEfpH6EoN5JaBUU-O67uv4v9nUIiWtpHCLA3_cSSg';
-const dashboard0ID = '6390be757de6d2fa567a3e34';
+const dashboard0ID = '6391d8972c8c733c64857525';
 
 require('dotenv').config(app.env);
-console.log(process.env);
+//console.log(process.env);
 
 /* 
 dummy user variables for tests
