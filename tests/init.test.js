a4bdc3c34d5672ff5325ce9d70393d7c879e642b
kmoratis <kmoratis@ece.auth.gr>
2022-12-10 13:22:42 +0200

    modified:   tests/init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -47,6 +47,42 @@ test('GET /statistics returns correct response and status code', async (t) => {
   t.is(statusCode, 200);
 });
 
+/*Test for the response and status code of get test-url */
+test('GET /test-url returns correct response and status code', async (t) => {
+  const test_url = "https://se2-frontend-9.netlify.app/";
+
+  const { body, statusCode } = await t.context.got(`general/test-url?url=${test_url}`);
+  t.assert(body.active);
+  t.is(statusCode, 200);
+});
+
+/*Test for the response and status code of get test-url, using wrong url */
+test('GET /test-url returns error status code if url is not valid', async (t) => {
+  const wrong_url = "htt://se2-frontend-9.netlify.app/";
+
+  const { body, statusCode } = await t.context.got(`general/test-url?url=${wrong_url}`);
+  t.assert(!body.active);
+  t.is(body.status, 500);
+});
+
+/*Test for the response and status code of get test-url-request */
+test('GET /test-url-request returns correct response and status code', async (t) => {
+  const wrong_url = "https://se2-frontend-9.netlify.app/";
+  const type = "GET";
+
+  const { body } = await t.context.got(`general/test-url-request?url=${wrong_url}&type=${type}`);
+  t.is(body.status, 200);
+});
+
+/*Test for the response and status code of get test-url-request using wrong request type url parameter */
+test('GET /test-url-request returns error status code if type is wrong', async (t) => {
+  const wrong_url = "https://se2-frontend-9.netlify.app/";
+  const type = "GT";
+
+  const { body } = await t.context.got(`general/test-url-request?url=${wrong_url}&type=${type}`);
+  t.is(body.status, 500);
+});
+
 /*Test for the response and status code of get sources*/
 test('GET /sources returns correct response and status code', async (t) => {
   const token = authToken;
