c9b88aa70c3590a82b65388a2aa11e685978251d
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-12 22:10:33 +0200

    Update ini.test.js

--- tests/ini.test.js
+++ tests/ini.test.js
@@ -6,14 +6,6 @@ const got = require('got');
 const listen = require('test-listen');
 
 const app = require('../src/index');
-const { jwtSign } = require('../src/utilities/authentication/helpers');
-const { AssertionError } = require('node:assert');
-//const { isAsyncFunction } = require('node:util/types');
-const authToken1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15IiwiaWQiOiI2MzhlNDA5MTMxMmRiMTRjNmVjMzBlNmYiLCJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImlhdCI6MTY3MDQxNjE5M30.g4hEfpH6EoN5JaBUU-O67uv4v9nUIiWtpHCLA3_cSSg';
-const authToken2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15MiIsImlkIjoiNjM4ZTRiM2QwNjE4ZTIyMTZjMGFjODMzIiwiZW1haWwiOiJkdW1teTJAZ21haWwuY29tIiwiaWF0IjoxNjcwODU1MTc5fQ.yHEuC1ssqKy0YUumB4G_krPZHwSFCviSE55MaJweWeA';
-
-const dashboard0ID = '63973c77b28f93494ec19fa3'; //belongs to dummy_user2
-const wrongdashID = '6390be757de6d2fa567a3e34';
 
 require('dotenv').config(app.env);
 //console.log(process.env);
