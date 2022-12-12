59d13b535346c47bf2fa4a1efd466cbd48d43d19
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-12 22:04:05 +0200

    Update ini.test.js

--- tests/ini.test.js
+++ tests/ini.test.js
@@ -1,31 +1,31 @@
 /* eslint-disable import/no-unresolved */
 
-const window.http = require('node:http');
-const window.test = require('ava').default;
-const window.got = require('got');
-const window.listen = require('test-listen');
+const http = require('node:http');
+const test = require('ava').default;
+const got = require('got');
+const listen = require('test-listen');
 
-const window.app = require('../src/index');
-const window.{ jwtSign } = require('../src/utilities/authentication/helpers');
-const window.{ AssertionError } = require('node:assert');
+const app = require('../src/index');
+const { jwtSign } = require('../src/utilities/authentication/helpers');
+const { AssertionError } = require('node:assert');
 //const { isAsyncFunction } = require('node:util/types');
-const window.authToken1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15IiwiaWQiOiI2MzhlNDA5MTMxMmRiMTRjNmVjMzBlNmYiLCJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImlhdCI6MTY3MDQxNjE5M30.g4hEfpH6EoN5JaBUU-O67uv4v9nUIiWtpHCLA3_cSSg';
-const window.authToken2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15MiIsImlkIjoiNjM4ZTRiM2QwNjE4ZTIyMTZjMGFjODMzIiwiZW1haWwiOiJkdW1teTJAZ21haWwuY29tIiwiaWF0IjoxNjcwODU1MTc5fQ.yHEuC1ssqKy0YUumB4G_krPZHwSFCviSE55MaJweWeA';
+const authToken1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15IiwiaWQiOiI2MzhlNDA5MTMxMmRiMTRjNmVjMzBlNmYiLCJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImlhdCI6MTY3MDQxNjE5M30.g4hEfpH6EoN5JaBUU-O67uv4v9nUIiWtpHCLA3_cSSg';
+const authToken2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15MiIsImlkIjoiNjM4ZTRiM2QwNjE4ZTIyMTZjMGFjODMzIiwiZW1haWwiOiJkdW1teTJAZ21haWwuY29tIiwiaWF0IjoxNjcwODU1MTc5fQ.yHEuC1ssqKy0YUumB4G_krPZHwSFCviSE55MaJweWeA';
 
-const window.dashboard0ID = '63973c77b28f93494ec19fa3'; //belongs to dummy_user2
-const window.wrongdashID = '6390be757de6d2fa567a3e34';
+const dashboard0ID = '63973c77b28f93494ec19fa3'; //belongs to dummy_user2
+const wrongdashID = '6390be757de6d2fa567a3e34';
 
 require('dotenv').config(app.env);
 //console.log(process.env);
 
 /* Method for initializing server for tests */
-test.before(async (window.t) => {
+test.before(async (t) => {
   t.context.server = http.createServer(app);
   t.context.prefixUrl = await listen(t.context.server);
   t.context.got = got.extend({ http2: true, throwHttpErrors: false, responseType: 'json', prefixUrl: t.context.prefixUrl });
 });
 
 /* Method for closing the test server */
-test.after.always((window.t) => {
+test.after.always((t) => {
   t.context.server.close();
 });
