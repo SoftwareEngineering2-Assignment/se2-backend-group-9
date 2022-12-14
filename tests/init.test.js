7490287673a015f3f3d8de9ced3beb8b237081b6
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-14 21:12:27 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -48,14 +48,14 @@ test('GET /test-url returns correct response and status code', async (t) => {
   t.is(statusCode, 200);
 });
 
-// /*Test for the response and status code of get test-url, using wrong url */
-// test('GET /test-url returns error status code if url is not valid', async (t) => {
-//   const wrong_url = "htt://se2-frontend-9.netlify.app/";
+/*Test for the response and status code of get test-url, using wrong url */
+test('GET /test-url returns error status code if url is not valid', async (t) => {
+  const wrong_url = "htt://se2-frontend-9.netlify.app/";
 
-//   const { body, statusCode } = await t.context.got(`general/test-url?url=${wrong_url}`);
-//   t.assert(!body.active);
-//   t.is(body.status, 500);
-// });
+  const { body, statusCode } = await t.context.got(`general/test-url?url=${wrong_url}`);
+  t.assert(!body.active);
+  t.is(body.status, 500);
+});
 
 /*Test for the response and status code of get test-url-request */
 test('GET /test-url-request returns correct response and status code', async (t) => {
@@ -66,14 +66,14 @@ test('GET /test-url-request returns correct response and status code', async (t)
   t.is(body.status, 200);
 });
 
-// /*Test for the response and status code of get test-url-request using wrong request type url parameter */
-// test('GET /test-url-request returns error status code if type is wrong', async (t) => {
-//   const wrong_url = "https://se2-frontend-9.netlify.app/";
-//   const type = "GT";
+/*Test for the response and status code of get test-url-request using wrong request type url parameter */
+test('GET /test-url-request returns error status code if type is wrong', async (t) => {
+  const wrong_url = "https://se2-frontend-9.netlify.app/";
+  const type = "GT";
 
-//   const { body } = await t.context.got(`general/test-url-request?url=${wrong_url}&type=${type}`);
-//   t.is(body.status, 500);
-// });
+  const { body } = await t.context.got(`general/test-url-request?url=${wrong_url}&type=${type}`);
+  t.is(body.status, 500);
+});
 
 /*Test for the response and status code of get sources*/
 test('GET /sources returns correct response and status code', async (t) => {
