0286a93111c0cc20f9f11b0fdef0587ba6e2aec9
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-14 21:07:33 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -8,7 +8,7 @@ const listen = require('test-listen');
 const app = require('../src/index');
 const { jwtSign } = require('../src/utilities/authentication/helpers');
 const { AssertionError } = require('node:assert');
-const { isAsyncFunction } = require('node:util/types');
+//const { isAsyncFunction } = require('node:util/types');
 
 require('dotenv').config(app.env);
 //console.log(process.env);
