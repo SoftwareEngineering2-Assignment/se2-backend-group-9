e3b45e3c232f6e9ef827263eb0bddad3564a8116
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-14 20:26:31 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -9,13 +9,20 @@ const app = require('../src/index');
 const { jwtSign } = require('../src/utilities/authentication/helpers');
 const { AssertionError } = require('node:assert');
 //const { isAsyncFunction } = require('node:util/types');
-const authToken1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15IiwiaWQiOiI2MzhlNDA5MTMxMmRiMTRjNmVjMzBlNmYiLCJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImlhdCI6MTY3MDQxNjE5M30.g4hEfpH6EoN5JaBUU-O67uv4v9nUIiWtpHCLA3_cSSg';
-const authToken2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15MiIsImlkIjoiNjM4ZTRiM2QwNjE4ZTIyMTZjMGFjODMzIiwiZW1haWwiOiJkdW1teTJAZ21haWwuY29tIiwiaWF0IjoxNjcwODU1MTc5fQ.yHEuC1ssqKy0YUumB4G_krPZHwSFCviSE55MaJweWeA';
+// const authToken1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15IiwiaWQiOiI2MzhlNDA5MTMxMmRiMTRjNmVjMzBlNmYiLCJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImlhdCI6MTY3MDQxNjE5M30.g4hEfpH6EoN5JaBUU-O67uv4v9nUIiWtpHCLA3_cSSg';
+// const authToken2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15MiIsImlkIjoiNjM4ZTRiM2QwNjE4ZTIyMTZjMGFjODMzIiwiZW1haWwiOiJkdW1teTJAZ21haWwuY29tIiwiaWF0IjoxNjcwODU1MTc5fQ.yHEuC1ssqKy0YUumB4G_krPZHwSFCviSE55MaJweWeA';
 
-const dashboard0ID = '63973c77b28f93494ec19fa3'; //belongs to dummy_user2
-const wrongdashID = '6390be757de6d2fa567a3e34';
+// const dashboard0ID = '63973c77b28f93494ec19fa3'; //belongs to dummy_user2
+// const wrongdashID = '6390be757de6d2fa567a3e34';
 
 require('dotenv').config(app.env);
+
+const authToken1 = app.env.AUTHTOKEN1;
+const authToken2 = app.env.AUTHTOKEN2;
+
+const dashboard0ID = app.env.DASHBOARD0ID; //belongs to dummy_user2
+const wrongdashID = app.env.WRONGDASHID;
+
 //console.log(process.env);
 
 /* 
