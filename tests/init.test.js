d6ccd0392493d3fe6bfc9a55d18808ce99247272
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-14 20:38:21 +0200

    Update init.test.js

--- tests/init.test.js
+++ tests/init.test.js
@@ -12,16 +12,12 @@ const { AssertionError } = require('node:assert');
 // const authToken1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15IiwiaWQiOiI2MzhlNDA5MTMxMmRiMTRjNmVjMzBlNmYiLCJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImlhdCI6MTY3MDQxNjE5M30.g4hEfpH6EoN5JaBUU-O67uv4v9nUIiWtpHCLA3_cSSg';
 // const authToken2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImR1bW15MiIsImlkIjoiNjM4ZTRiM2QwNjE4ZTIyMTZjMGFjODMzIiwiZW1haWwiOiJkdW1teTJAZ21haWwuY29tIiwiaWF0IjoxNjcwODU1MTc5fQ.yHEuC1ssqKy0YUumB4G_krPZHwSFCviSE55MaJweWeA';
 
-// const dashboard0ID = '63973c77b28f93494ec19fa3'; //belongs to dummy_user2
-// const wrongdashID = '6390be757de6d2fa567a3e34';
-
 require('dotenv').config(app.env);
 
 const authToken1 = process.env.AUTHTOKEN1;
 const authToken2 = process.env.AUTHTOKEN2;
-
-const dashboard0ID = process.env.DASHBOARD0ID; //belongs to dummy_user2
-const wrongdashID = process.env.WRONGDASHID;
+const dashboard0ID = '63973c77b28f93494ec19fa3'; //belongs to dummy_user2
+const wrongdashID = '6390be757de6d2fa567a3e34';
 
 //console.log(process.env);
 
