27c04905864452e873fdc64534ca50ae40dde1c7
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-28 13:24:58 +0200

    Added documentation

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -7,17 +7,27 @@ module.exports = (req, res, next) => {
   /**
      * @name authorization
      * @description Middleware that checks a token's presence and validity in a request
+     * needs secret as input
     */
   pipe(
     (r) =>
       path(['query', 'token'], r)
           || path(['headers', 'x-access-token'], r)
           || path(['headers', 'authorization'], r),
+    
+    /**
+      * if token in not null and is a Bearer token, then return it sliced
+      * else return it as it is.
+     */
     ifElse(
       (t) => !isNil(t) && startsWith('Bearer ', t),
       (t) => slice(7, t.length, t).trimLeft(),
       identity
     ),
+    /**
+      * if token in null it returns error code (403) and token missing message,
+      * else call jwt.verify to verify token
+     */
     ifElse(
       isNil,
       () =>
@@ -26,6 +36,11 @@ module.exports = (req, res, next) => {
           status: 403
         }),
       (token) =>
+        /**
+          * if verify returns null error, token is valid and it returns the decoded request,
+          * else if error is not null, and its name is TokenExpiredError then return error code (401) and expired message,
+          * else return error code (403) and failed to verify token
+         */
         jwt.verify(token, secret, (e, d) =>
           ifElse(
             (err) => !isNil(err),
