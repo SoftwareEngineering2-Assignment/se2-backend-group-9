72188d2b4342aab8c16ef966856af9aac14c24b9
kmoratis <73110291+kmoratis@users.noreply.github.com>
2022-12-28 12:21:38 +0200

    Added documentation

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,3 +1,6 @@
+/*
+File for mongoose database configuration
+*/
 const mongoose = require('mongoose');
 
 const mongooseOptions = {
@@ -10,7 +13,12 @@ const mongooseOptions = {
   keepAliveInitialDelay: 300000
 };
 const mongodbUri = process.env.MONGODB_URI;
-  
+ 
+/*
+Function for connecting to mongoose database
+Needs mongodbUri, mongooseOptions as inputs
+Exports the connected database as output
+*/
 module.exports = () => {
   // eslint-disable-next-line no-console
   mongoose.connect(mongodbUri, mongooseOptions).catch(console.error);
